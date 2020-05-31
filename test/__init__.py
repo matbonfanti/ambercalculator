f85eb21ae85e0f97be356ce9206d689c02bf9b2d
Matteo Bonfanti <mat.bonfanti@gmail.com>
2020-05-31 18:37:14 +0200

    file for importing test directory

---
+++ test/__init__.py
@@ -0,0 +1,4 @@
+from test.amberinput import TestAmberInput
+from test.amberoutput import TestAmberOutput
+from test.ambersnapshot import TestAmberSnapshot
+from test.ambertopology import TestAmberTopology 
